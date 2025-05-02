cfc4c61d8529468d9a12a946977a8f947657dcca
TharushiS23 <tahrushimaliksha@gmail.com>
2025-05-03 02:21:32 +0530

    Changing to correct variable names

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 # Initialize session state
-if 'appcode' not in st.session_state:
-    st.session_state['appcode'] = False
+if 'app' not in st.session_state:
+    st.session_state['app'] = False
 
 # Determine which page to show
-if st.session_state['appcode']:
+if st.session_state['app']:
     # Import and run your existing dashboard
     import app
     app.main()  # This runs your existing dashboard code
