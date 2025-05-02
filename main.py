8eddf3c16eac88852a35660fce4e23725a076361
TharushiS23 <tahrushimaliksha@gmail.com>
2025-05-03 00:15:06 +0530

    Correcting the error in the main.py file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if 'Main_app' not in st.session_state:
 if st.session_state['Main_app']:
     # Import and run your existing dashboard
     import app
-    app.main()  # This runs your existing dashboard code
+    app.load_data()  # This runs your existing dashboard code
 else:
     # Import and run the game/loading page
     from loadingpage import loading_page
