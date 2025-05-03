3890d17759d1d2f566e0402975f59b93be2b2801
TharushiS23 <tahrushimaliksha@gmail.com>
2025-05-03 14:38:25 +0530

    Renaming the files to remove the confusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from loading import show_loading_game
-from app import main  # assuming your dashboard is also in app.py (see note below)
+from dashboard import main  
 
 # Run app or game based on session state
 if 'appcode' not in st.session_state:
